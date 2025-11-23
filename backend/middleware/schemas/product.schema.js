74a4d956b19cd18a261f923a349f58666a0ba10d
MOHAMedelhennawy <elhennawy2002@gmail.com>
2025-11-23 20:26:12 +0200

    Move all backend files to backend dir

--- backend/middleware/schemas/product.schema.js
+++ backend/middleware/schemas/product.schema.js
@@ -1,3 +1,43 @@
+const schema = {
+    type: 'object',
+    properties: {
+        name: {
+            type: 'string',
+            minLength: 5,
+            maxLength: 20,
+        },
+        category_id: {
+            type: 'string',
+            isValidCategory: true,
+        },
+        subCategory_id: {
+            type: 'string',
+            isValidSubCategory: true
+        },
+        description: {
+            type: 'string',
+            minLength: 10,
+            maxLength: 2000,
+        },
+        summary: {
+            type: 'string',
+            minLength: 10,
+            maxLength: 100,
+
+        },
+        price: {
+            type: 'string',
+            priceIsFloat: true,
+        },
+        quantity: {
+            type: 'string',
+            quantityIsNumber: true,
+        },
+    },
+    required: ['name', 'category_id', 'subCategory_id', 'description', 'summary', 'price', 'quantity'],
+    additionalProperties: false,
+};
+
 export const productSchemaPost = {
     type: 'object',
     properties: {
@@ -76,3 +116,5 @@ export const productSchemaPut = {
     },
     additionalProperties: false,
 };
+
+export default schema;
--- backend/routes/productsRouter.js
+++ backend/routes/productsRouter.js
@@ -1,15 +1,13 @@
 import express from "express";
 import upload from '../config/multer.js';
-import { checkUser, checkAdmin } from "../middleware/authMWPermission.js";
+import { checkAdmin, authenticate } from "../middleware/authMWPermission.js";
 import { poductMWvalidator } from "../middleware/productMWvalidators.js";
 import { addNewProduct, getAllProducts, getProductById } from "../controller/productsController.js";
-import { validateSchema } from "../middleware/validateSchema.js";
-import { productSchemaPost } from "../schemas/productSchema.js";
 
 const router = express.Router();
 
 router.get('/', getAllProducts);
 router.get('/:id', getProductById)
-router.post('/', checkUser, checkAdmin, upload.single('image'), validateSchema(productSchemaPost), addNewProduct);
+router.post('/', authenticate, checkAdmin, upload.single('image'), poductMWvalidator, addNewProduct);
 
 export default router;
